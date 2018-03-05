98449238da723f65876dfa7d79d4c1cea2a72b0b
Keith Ward <trueinviso@gmail.com>
2018-03-04 17:36:46 -0800

    import babel-polyfill

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 // Support component names relative to this directory:
+import "babel-polyfill"
 var componentRequireContext = require.context("components", true)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
