fe8bbee8a59762687769376cc0b20a5c4963be85
Keith Ward <trueinviso@gmail.com>
2019-07-01 14:37:43 -0700

    Return correct thumbnail.

--- app/controllers/api/v1/thumbnails_controller.rb
+++ app/controllers/api/v1/thumbnails_controller.rb
@@ -2,19 +2,19 @@ module Api
   module V1
     class ThumbnailsController < ApplicationController
       def update
-        ThumbnailUpdater.call!(current_user, update_params)
-        render status: 200, json: {
-          thumbnail: thumbnail_for(params[:model_type], params[:record_id]),
-        }
+        record = ThumbnailUpdater.call!(current_user, update_params)
+        if record
+          render status: 200, json: {
+            thumbnail: record.thumbnail_url, params[:record_id]),
+          }
+        else
+          render status: 400, json: {
+            errors: "There was a problem updating the thumbnail.",
+          }
+        end
       end
-
-      
 
       private
-
-      def thumbnail_for(type, id)
-        type.camelize.constantize.find(id).thumbnail_url
-      end
 
       def update_params
         params.permit(
