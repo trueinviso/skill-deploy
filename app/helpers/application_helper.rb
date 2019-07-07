3bc997c99a479ff96720f25af8c8f4d7cf88e023
Keith Ward <trueinviso@gmail.com>
2019-07-07 15:36:47 -0700

    Move new billing info to correct unity template.
    Modify nav links to work with unity engine.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,30 +1,30 @@
 module ApplicationHelper
   def employer_jobs_page?
-    current_page?(employer_jobs_path)
+    current_page?(main_app.employer_jobs_path)
   end
 
   def edit_profile_page?
-    current_page?(edit_user_registration_path)
+    current_page?(main_app.edit_user_registration_path)
   end
 
   def edit_social_profile_page?
-    current_page?(edit_profile_social_path)
+    current_page?(main_app.edit_profile_social_path)
   end
 
   def edit_notifications_page?
-    current_page?(edit_profile_notifications_path)
+    current_page?(main_app.edit_profile_notifications_path)
   end
 
   def saved_jobs_page?
-    current_page?(favorite_jobs_path)
+    current_page?(main_app.favorite_jobs_path)
   end
 
   def applied_fors_page?
-    current_page?(applied_fors_path)
+    current_page?(main_app.applied_fors_path)
   end
 
   def employer_billing_page?
-    current_page?(edit_employer_billing_path)
+    current_page?(main_app.edit_employer_billing_path)
   end
 
   def embedded_svg filename, options={}
