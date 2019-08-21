82f7bd5f78b7a7162183b775c651b7393c18206e
polyglot.me <polyglot.me1987@gmail.com>
2019-08-21 20:55:36 +0530

    Refactor employer job policy specs.

--- spec/policies/employer/job_policy_spec.rb
+++ spec/policies/employer/job_policy_spec.rb
@@ -6,17 +6,55 @@ module Employer
     let(:job) { build(:job) }
 
     context 'employer' do
-      let(:user) { create(:employer) }
+      context 'With unsubscribed employer' do
+        let(:user) { create(:employer) }
 
-      it "forbid all actions" do
-        is_expected.to forbid_actions([
-          :index,
-          :new,
-          :create,
-          :edit,
-          :update,
-          :destroy
-        ])
+        it "forbid all actions" do
+          is_expected.to forbid_actions([
+            :index,
+            :new,
+            :create,
+            :edit,
+            :update,
+            :destroy
+          ])
+        end
+      end
+
+      context 'active paid employer' do
+        let(:user) { create(:employer, :active_subscriber) }
+
+        it "forbid actions" do
+          is_expected.to forbid_actions([
+            :edit,
+            :update,
+            :destroy
+          ])
+        end
+
+        it "permit actions" do
+          is_expected.to permit_actions([
+            :index,
+            :new,
+            :create,
+          ])
+        end
+      end
+
+      context 'owner with active paid subscription' do
+        let(:user) { create(:employer, :active_subscriber) }
+        before { job.user_id = user.id }
+
+        it "permit all actions" do
+          is_expected.to permit_actions([
+            :index,
+            :new,
+            :create,
+            :edit,
+            :update,
+            :destroy
+          ])
+        end
       end
     end
 
@@ -25,42 +63,6 @@ module Employer
 
       it "forbid all actions" do
         is_expected.to forbid_actions([
-          :index,
-          :new,
-          :create,
-          :edit,
-          :update,
-          :destroy
-        ])
-      end
-    end
-
-    context 'active paid employer' do
-      let(:user) { create(:employer, :active_subscriber) }
-
-      it "forbid actions" do
-        is_expected.to forbid_actions([
-          :edit,
-          :update,
-          :destroy
-        ])
-      end
-
-      it "permit actions" do
-        is_expected.to permit_actions([
-          :index,
-          :new,
-          :create,
-        ])
-      end
-    end
-
-    context 'owner with active paid subscription' do
-      let(:user) { create(:employer, :active_subscriber) }
-      before { job.user_id = user.id }
-  
-      it "permit all actions" do
-        is_expected.to permit_actions([
           :index,
           :new,
           :create,
