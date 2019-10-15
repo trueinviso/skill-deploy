74b5f166031b62e80df2345fbbf35268a4fa0e1a
Keith Ward <trueinviso@gmail.com>
2019-10-14 18:18:10 -0700

    Add thumbnail to job card.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,5 @@
 class JobsController < ApplicationController
   def index
-    @jobs = jobs.sort { |x, y| y.created_at <=> x.created_at }
     @view_component = JobsComponent.new(current_user, jobs)
   end
 
@@ -11,10 +10,9 @@ class JobsController < ApplicationController
   private
 
   def jobs
-    if params[:search].present?
-      Job.filter(params.slice(:search))
-    else
-      Job.filter(params.slice(:job_type_name, :job_role_name))
-    end
+    JobsSerializer.build(
+      current_user,
+      JobsQuery.new(params),
+    )
   end
 end
