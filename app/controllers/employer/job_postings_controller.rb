dc75a4ee4fc076a0664ab2192ba4147f9f80c7f1
polyglot.me <polyglot.me1987@gmail.com>
2019-10-22 22:24:57 +0530

    Add link in the flash message.

--- app/controllers/employer/job_postings_controller.rb
+++ app/controllers/employer/job_postings_controller.rb
@@ -4,8 +4,14 @@ module Employer
       @job = Job.find(params[:id])
       authorize [:employer, @job]
       @job.published!
-      flash[:notice] = "Congratulations! your listing is now live. View your listing"
+      flash[:notice] = "<span>Congratulations! your listing is now live.</span> #{redirect_link}"
       redirect_to employer_jobs_path
+    end
+
+    private
+
+    def redirect_link
+      view_context.link_to("View your listing", job_path(@job))
     end
   end
 end
