8cb47d9da6cf1526634645afb663331082e72c07
Arthur <arthur.nesterenko.dev@gmail.com>
2021-04-16 15:48:35 +0300

    fix: mistakes after rebase

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,21 @@ class ApplicationController < ActionController::Base
   before_action :guard_user_authenticated!,
                 :guard_user_registered!,
                 :guard_user_profile_reviewed!, unless: :devise_controller?
+
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   # Available from Devise engine
+  
   def after_sign_in_path_for(resource)
     return [:new, :session] unless user_signed_in?
+    
     stored_location_for(resource) || Receptionist.new(resource).direct
   end
 
   private
 
   def guard_user_profile_reviewed!
-    if current_user.pending?
+    if current_user.user_profile.pending?
       redirect_to profile_pending_review_path
     end
   end
