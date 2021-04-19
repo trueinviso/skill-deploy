4c1d6b90f17c04c412d2380d35cbd1ea04f1b247
lkibbalam <lkibbalamlx@gmail.com>
2021-04-19 14:41:32 +0300

    dh-job-listing-statuses#add job create allready published job listing

--- app/controllers/employer/job_postings_controller.rb
+++ app/controllers/employer/job_postings_controller.rb
@@ -1,5 +1,16 @@
 module Employer
   class JobPostingsController < ApplicationController
+
+    def create
+      authorize [:employer, Job]
+      @job = Job.new(valid_params[:job].merge!(user_id: current_user.id, status: :published))
+      if @job.save
+        redirect_to employer_preview_job_path(@job)
+      else
+        render :new
+      end 
+    end
+
     def update
       @job = Job.find(params[:id])
       authorize [:employer, @job]
@@ -12,6 +23,30 @@ module Employer
 
     def redirect_link
       view_context.link_to("View your listing", job_path(@job))
+    end
+
+    def valid_params
+      params
+        .permit(
+          :id,
+          job: [
+            :company_name,
+            :company_website,
+            :contact_email,
+            :contact_name,
+            :description,
+            :job_role_ids,
+            :job_type_ids,
+            :job_experience_ids,
+            :location,
+            :name,
+            :remote,
+            :twitter,
+            :facebook,
+            :instagram,
+            thumbnail_attributes: [:file],
+          ],
+        )
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ Rails.application.routes.draw do
     root to: "jobs#index"
 
     resources :jobs
-    resources :job_postings, only: :update
+    resources :job_postings, only: %i[create update]
 
     namespace :preview do
       resources :job,
