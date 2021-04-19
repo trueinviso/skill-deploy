eddd1dc4ae0d2c85c0c65d961dd9ccebc5dc2a02
lkibbalam <lkibbalamlx@gmail.com>
2021-04-19 20:48:12 +0300

    ps-update-for-job-listing-creation#fix of spacing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,13 +3,13 @@ require "spec_helper"
 require "capybara/rspec"
 Capybara.javascript_driver = :selenium_headless
 Capybara.server = :puma, { Silent: true }
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
+require "rspec/rails"
 
-Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
