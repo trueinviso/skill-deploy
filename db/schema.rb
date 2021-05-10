86f9f2b19f0ec17c7daa30de92053db0710c2eb3
lkibbalam <lkibbalamlx@gmail.com>
2021-05-10 20:45:32 +0300

    profile-form-fixes# add confirmable devise module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_141947) do
+ActiveRecord::Schema.define(version: 2021_05_10_172127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -375,7 +375,12 @@ ActiveRecord::Schema.define(version: 2021_04_27_141947) do
     t.string "provider"
     t.string "uid"
     t.bigint "rate_range_id"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["rate_range_id"], name: "index_users_on_rate_range_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
