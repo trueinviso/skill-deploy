b7b76bbfeea0d9bd898249ea8c61d830fae34a6d
lkibbalam <lkibbalamlx@gmail.com>
2021-05-12 23:40:49 +0300

    dh-profile-form-fixes#user can have only one role validation

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,3 +1,13 @@
 class WorkExperience < ApplicationRecord
   belongs_to :user
+
+  validate :only_one_current_role
+
+  private
+
+  def only_one_current_role
+    if user.work_experiences.count(&:current_role) > 1
+     errors.add(:user, "can have only one current role")
+    end
+  end
 end
