bee84c73da418b25e1ffd14e2c8897d71fef78bd
programming-enthusiast <jerry.lee.dev@outlook.com>
2021-05-18 21:17:23 +0100

    Add banner and flag for profile edit page

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -26,6 +26,9 @@ class UserProfilesController < ApplicationController
 
   def edit
     @user_profile = current_user.user_profile
+    if current_user.employer? && !current_user.talent?
+      @banner_message = "You'll need to submit your profile for review before applying for jobs."
+    end
   end
 
   def show
