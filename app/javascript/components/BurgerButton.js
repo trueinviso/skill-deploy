2d6ff1120e8e583083543d1af35b6cfaa3484b61
programming-enthusiast <jerry.lee.dev@outlook.com>
2021-05-21 15:33:24 -0500

    Fix BurgerButton.js

--- app/javascript/components/BurgerButton.js
+++ app/javascript/components/BurgerButton.js
@@ -1,39 +1,36 @@
 import attachEvent from "~/helpers/attachEvent";
 
-const TRIGGER_EVENT_BURGER_BUTTON_SELECTOR = "burger-button-trigger-event"
-
 function burgerButton() {
-  const burgerButtons = [
-    ...document.querySelectorAll(`[data-${TRIGGER_EVENT_BURGER_BUTTON_SELECTOR}]`)
-  ].map(burgerButton => {
-    const event = burgerButton.getAttribute(`data-${TRIGGER_EVENT_BURGER_BUTTON_SELECTOR}`)
+  const burgerButton = document.getElementById('burger-button')
+  const menuCloseButton = document.getElementById("menu-close-button")
 
-    if (event) {
-      return attachEvent(
-          event,
-          () => {
-            const burgerButton = document.getElementById("burger-button")
-            const menuCloseButton = document.getElementById("menu-close-button")
-            const mobileSidebarContainer = document.getElementById("mobile-sidebar__container")
-            const mobileSidebar = document.getElementById("mobile-sidebar")
+  const toggleBurgerMenu = () => {
+    const mobileSidebarContainer = document.getElementById("mobile-sidebar__container")
+    const mobileSidebar = document.getElementById("mobile-sidebar")
 
-            if (mobileSidebarContainer) {
-              burgerButton.classList.toggle("hidden")
-              menuCloseButton.classList.toggle("hidden")
-              mobileSidebarContainer.classList.toggle("hidden")
-              mobileSidebar.classList.toggle("active")
-            }
-          },
-          burgerButton
-      )
+    if (mobileSidebarContainer) {
+      burgerButton.classList.toggle("hidden")
+      menuCloseButton.classList.toggle("hidden")
+      mobileSidebarContainer.classList.toggle("hidden")
+      mobileSidebar.classList.toggle("active")
     }
-    return () => {}
-  })
+  }
 
-  return function cleanupForm() {
-    burgerButtons.forEach(remove => {
-      remove()
-    })
+  const removeBurgerButtonEvent =  attachEvent(
+    'click',
+    toggleBurgerMenu,
+    burgerButton
+  )
+
+  const removeCloseButtonEvent =  attachEvent(
+      'click',
+      toggleBurgerMenu,
+      menuCloseButton
+  )
+  
+  return function cleanup() {
+    removeBurgerButtonEvent();
+    removeCloseButtonEvent();
   }
 }
 
