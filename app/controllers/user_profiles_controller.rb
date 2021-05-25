aac8f15f928dcdd8cbde112dd0c6fc832ed97789
Keith <trueinviso@gmail.com>
2021-05-25 08:53:32 -0700

    Only show apply label for talent role.
    Add talent_only? method to authorized concern.

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -26,7 +26,7 @@ class UserProfilesController < ApplicationController
 
   def edit
     @user_profile = current_user.user_profile
-    if current_user.employer? && !current_user.talent?
+    if current_user.employer_only?
       flash[:banner_message] = "You'll need to submit your profile for review before applying for jobs."
     end
   end
