5470dd741d49276b16c28cc67a377e1b3f171b83
Keith <trueinviso@gmail.com>
2021-06-17 20:50:00 -0700

    Show pending profile flash message on profile edit screen.

--- app/policies/protocols/show_apply_label.rb
+++ app/policies/protocols/show_apply_label.rb
@@ -6,12 +6,5 @@ module Protocols
       user.pending_talent? ||
         user.employer_only?
     end
-
-    private
-
-    def employer_without_profile?(user)
-      user.employer_only? &&
-        user.user_profile.blank?
-    end
   end
 end
