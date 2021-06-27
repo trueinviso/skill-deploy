d517b0a693d51ee6779032dd0e871f80854bf471
Keith <trueinviso@gmail.com>
2021-06-26 19:37:07 -0700

    Deactivate user listings after subscription cancel.
    Only show published listings on the index page.

--- app/models/jobs_query.rb
+++ app/models/jobs_query.rb
@@ -7,6 +7,7 @@ class JobsQuery
 
   def jobs
     @jobs ||= Job
+      .published
       .includes(:job_types)
       .filter_result(params.slice(:job_type_name, :job_role_name, :search))
       .sort { |x, y| y.created_at <=> x.created_at }
--- app/services/subscription_syncer.rb
+++ app/services/subscription_syncer.rb
@@ -17,9 +17,19 @@ class SubscriptionSyncer
     subscription.update!(
       gateway_status: gateway_subscription.status,
       trial_ends_at: gateway_subscription.trial_end,
-      cancellation_date: gateway_subscription.cancel_at,
-      marked_for_cancellation_at: gateway_subscription.canceled_at,
+      cancellation_date: cancellation_date,
+      marked_for_cancellation_at: marked_for_cancellation_at,
     )
+  end
+
+  def cancellation_date
+    return nil if gateway_subscription.cancel_at.blank?
+    Time.at(gateway_subscription.cancel_at)
+  end
+
+  def marked_for_cancellation_at
+    return nil if gateway_subscription.canceled_at.blank?
+    Time.at(gateway_subscription.canceled_at)
   end
 
   def gateway_subscription
@@ -29,7 +39,12 @@ class SubscriptionSyncer
 
   def remove_cancelled_subscription
     if subscription.reload.canceled?
+      deactivate_job_listings
       subscription.destroy
     end
+  end
+
+  def deactivate_job_listings
+    subscription.user.jobs.map(&:archived!)
   end
 end
