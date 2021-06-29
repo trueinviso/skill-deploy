64af70095c5977e2043ee1085913dfc0ee0c6bb5
Keith <trueinviso@gmail.com>
2021-06-29 15:38:58 -0700

    Add application confirmation email.

--- app/controllers/applied_fors_controller.rb
+++ app/controllers/applied_fors_controller.rb
@@ -43,12 +43,20 @@ class AppliedForsController < ApplicationController
   def dynamic_template_data
     {
       name: current_user.first_name,
-      headline: job.name,
+      job_listing_name: job.name,
       about: job.description&.body&.to_plain_text,
-      skills: current_user.skills,
-      avatar_url: current_user.thumbnail_url,
-      profile_url: employer_user_profile_url(current_user),
-      website_url: root_url,
+      skills: skills,
+      website_url: job.company_website,
+      logo_url: job.thumbnail_url,
     }
+  end
+
+  def skills
+    [
+      job.job_roles.first&.name,
+      job.job_types.first&.name,
+      job.job_experiences.first&.name,
+      job.job_locations.first&.name,
+    ]
   end
 end
--- app/services/sendgrid_manager.rb
+++ app/services/sendgrid_manager.rb
@@ -7,12 +7,13 @@ module SendgridManager
 
   TEMPLATE_IDS = {
     published_job_notification: "d-d746e27796cc4f13b153e4188da91d99",
-    apply_notification: "d-2d778090ddcb43df8eec4e5241279503",
+    apply_notification: "d-3f9748fca35f4e58a2712ad010362665",
     password_change: "d-77437192fa3e4c2083b992c444575f4e",
     reset_password_instructions: "d-e021181d93c44d0ab3143ff5bf7d81d6",
     recruiter_message: "d-c5d9bd7f8a51475ea8852e84b481d25d",
     cancel_subscription: "d-c54674fd878749f3acf98688cb82d649",
     subscription_receipt: "d-bb4ebaae730d4c36b14f5a9a7b7514c7",
+    confirmation_instructions: "d-7d45ed2e3da64e0b8d8b544342c7d7ba",
   }
 
   def send(to, template_id, subsitutions = {})
