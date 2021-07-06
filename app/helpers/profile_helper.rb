5acb9e7e472a4309031aa9023fe1479a46582f66
Keith <trueinviso@gmail.com>
2021-07-06 09:58:05 -0700

    Sort rate ranges by from field.

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,6 +1,6 @@
 module ProfileHelper
   def rate_range_options
-    RateRange.decorate.map do |rr|
+    RateRange.decorate.order(:from).map do |rr|
       { id: rr.id, from: rr.from, to: rr.to, label: rr.label }
     end
   end
